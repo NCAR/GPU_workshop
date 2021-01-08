66ba7acc2961eb78c1116f8b4f881c196af6ac1f
supreethms1809 <supreethms1809@gmail.com>
2021-01-08 14:38:35 -0700

    common.cpp - Contains the common functions necessary for initialize and verification
    Adding common.o inside the makefile, and declared the common functions in the headerfile

--- Lesson_Briley/Wkshp_head.h
+++ Lesson_Briley/Wkshp_head.h
@@ -1,7 +1,4 @@
 
-#ifndef Wkshp_head 
-#define Wkshp_head
-
 //Add headers that you want to pre-compile here 
 #include <stdio.h>
 #include <time.h> 
@@ -12,7 +9,12 @@ void InitializeM(float *array, const int ny, const int nx,const float val);
 
 void cpuMMult(float *A, float *B, float *C, const int nx, const int ny);
 
+//Function declaration in common 
+void InitializeMatrixSame(float *array, const int ny, const int nx, const float val);
+void InitializeMatrixRand(float *array, const int ny, const int nx);
+
+void MatrixVerification(float *hostC, float *gpuC, const int ny, const int nx, const float fTolerance);
+
+
 //GPU Implementations
 extern void gpuMult(float *h_A, float *h_B, float *h_C, const int ny, const int nx);
-
-#endif
--- Lesson_Briley/submit.sh
+++ Lesson_Briley/submit.sh
@@ -5,9 +5,11 @@
 #SBATCH --account NTDD0002
 #SBATCH --partition=dav
 #SBATCH --reservation=casper_8xV100
+#SBATCH --gres=gpu:v100:1
 #SBATCH --time=00:15:00
 #SBATCH --output=log.FMA_%j.out
 #SBATCH --job-name=GPU_FMA
+#SBATCH --mem=50G
 
 # Load the necessary modules (software)
 module purge
@@ -19,7 +21,9 @@ module list
 # Update LD_LIBRARY_PATH so that cuda libraries can be found
 export LD_LIBRARY_PATH=${NCAR_ROOT_CUDA}/lib64:${LD_LIBRARY_PATH}
 echo ${LD_LIBRARY_PATH}
+nvidia-smi
 
 # Move to the correct directory and run the executable
 echo -e "\nBeginning code output:\n-------------\n"
 srun ./fma.exe
+#./fma.exe
