be6818f22a76a29a7dba71072f16047b94b09964
Evan MacBride <ejmacbride@gmail.com>
2021-01-11 23:57:44 +0000

    undo build.sh changes

--- lesson_evan/solution/build.sh
+++ lesson_evan/solution/build.sh
@@ -2,16 +2,13 @@
 # Changed some lines for running on UD CRPL machines
 # Load the necessary modules (software)
 module purge
-#module load ncarenv/1.2
-#module load nvhpc/20.11
-module load nvhpc/20.7
-#module load cuda/11.0.3
-module load cuda/10.2 # Changed to run on UD CRPL machines
+module load ncarenv/1.2
+module load nvhpc/20.11
+module load cuda/11.0.3
 module list
 
 # Export variables for use in the Makefile
-#export CUDA_ROOT_PATH="${NCAR_ROOT_CUDA}"
-export CUDA_ROOT_PATH=/opt/cuda/10.2
+export CUDA_ROOT_PATH="${NCAR_ROOT_CUDA}"
 
 # Remove any previous build attempts
 make clean
