a40bf43bc43717456d14b29570c3435aaa57126f
supreethms1809 <supreethms1809@gmail.com>
2021-01-25 11:30:56 -0700

    Restructuring the directory
    Restructured the directory to separate exercise and solution folder.
    The exercise consists of the comments on where they need to add the code.

---
+++ Lesson_1_matrixAdd/CUDA/exercise/matrix_add.cu
@@ -0,0 +1,62 @@
+#include <cuda_runtime.h>
+#include <cuda.h>
+#include "pch.h"
+#include <stdio.h>
+ 
+//Add the keyword to make it a GPU function
+ void add_matrices(float* m1, float* m2, float* sum, int m, int n){
+
+    // Calculate the thread idx and idy index 
+    
+
+    // Calculate the Addition
+
+
+}
+
+//Add the keyword to make it a host function
+ void gpu_matrix_add(const float *h_A, const float *h_B, float *h_C,\
+   const int dx, const int dy)
+{
+  float *d_A, *d_B, *d_C;
+
+  // Allocate memory on GPU for the matrices
+
+  // Check for any errors
+  cudaCheckErrors("cudaMalloc failure");
+
+  // Copy the matrices A, B from host to device
+
+
+
+  cudaCheckErrors("cudaMemcpy H2D failture");
+
+  // Kernel Invoke Paramters (2D grid and blocks) 
+  // Decide the dimensions of a block
+  int dimx = 16; 
+  int dimy = 16; 
+  // Block of 256 threads 
+  dim3 block(dimx, dimy); 
+
+  /* Calculate the grid dimensions based on the dimensions of the matrix,
+    and dimensions of the block */
+  dim3 grid((dx+block.x-1)/block.x, (dy+block.y-1)/block.y); 
+
+  // Calcuate A+B=C on the GPU
+
+
+  cudaCheckErrors("kernel launch failure");
+
+  // Block the CPU until GPU finishes execution
+
+
+  // Copy the result matrix C from device to host
+
+  cudaCheckErrors("Kernel execution failure or cudaMemcpy H2D failure");
+
+  // deallocate memory on GPU
+  cudaCheckErrors("cudaFree failure");
+
+  // Reset Device
+  cudaDeviceReset();
+}
