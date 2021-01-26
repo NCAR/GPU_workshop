3852fe722724c63b3c40b9e4f2f6d655d6887d27
Evan MacBride <ejmacbride@gmail.com>
2021-01-26 14:25:31 -0700

    divide code into exercise and solution directories
    Create new subdirectories within CUDA and OpenACC directories for
    exercise and solution code. For now, the two CUDA subdirectories are
    identical to each other, as are the two OpenACC subdirectories. A
    next step will be to alter the exercise code by adding, removing, or
    otherwise changing parts of it.

---
+++ Lesson_Evan/CUDA/solution/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Load the necessary modules (software)
+module purge
+module load ncarenv/1.2
+module load nvhpc/20.11
+module load cuda/11.0.3
+module list
+
+# Export variables for use in the Makefile
+export CUDA_ROOT_PATH="${NCAR_ROOT_CUDA}"
+
+# Remove any previous build attempts
+make clean
+# Do a build
+make 
