069de57ca118297bb5f3f283a9ea4e35617b871e
cenamiller <cena.miller@gmail.com>
2021-01-27 19:47:08 -0700

    MatAdd OpenACC Debug Challenge. Provided output/error files for 5 test cases w/ only difference being the Matrix dimensions. One runs successfully, three fail verification, and the last one hangs and reports a memory issue.

---
+++ Lesson_1_matrixAdd/OpenACC/debug1/matrix_add.cc
@@ -0,0 +1,19 @@
+#include <openacc.h>
+#include "pch.h"
+#include <stdio.h>
+
+
+// Calculate A+B=C on the device
+void openacc_matrix_add(const float *o_A, const float *o_B, float *o_C, const int rows, \
+const int cols) {
+  #pragma acc data copyin(o_A[0:rows*cols], o_B[0:rows*cols]), copy(o_C[0:rows*cols]) 
+  {
+    #pragma acc parallel loop collapse(2)
+    for (int i = 0; i < rows; i++){
+      for (int j = 0; j < cols; j++) {
+        int idx = i*rows+j;
+        o_C[idx] = o_A[idx] + o_B[idx];
+      }
+    }
+  }
+}
