b61c3b2d063acd2c94f3235585023612ccec7989
cenamiller <cena.miller@gmail.com>
2021-02-02 08:10:30 -0700

    OpenACC-C Matrix Add
        Changed copy->copyout for result matrix in solution

--- Lesson_1_matrixAdd/OpenACC/solution/matrix_add.cc
+++ Lesson_1_matrixAdd/OpenACC/solution/matrix_add.cc
@@ -6,7 +6,7 @@
 // Calculate A+B=C on the device
 void openacc_matrix_add(const float *o_A, const float *o_B, float *o_C, const int rows, \
 const int cols) {
-  #pragma acc data copyin(o_A[0:rows*cols], o_B[0:rows*cols]), copy(o_C[0:rows*cols]) 
+  #pragma acc data copyin(o_A[0:rows*cols], o_B[0:rows*cols]) copyout(o_C[0:rows*cols]) 
   {
     #pragma acc parallel loop collapse(2)
     for (int i = 0; i < rows; i++){
