e2c5c2dfad466e49c0c9682c01a7897de92f44be
Evan MacBride <ejmacbride@gmail.com>
2021-02-03 14:18:22 -0700

    adjust syntax to match earlier matmul example
    Rewrite the OpenACC directives and clauses in matmul.cc to mirror what
    was used in the matrix multiplication example from Session 1.

--- session2/Lesson_Evan/OpenACC/exercise/matmul.cc
+++ session2/Lesson_Evan/OpenACC/exercise/matmul.cc
@@ -2,16 +2,18 @@
 
 void gpuMatmul(const float *A, const float *B, float *C, const int m, const int p, const int q) {
   float temp = 0.0;
-#pragma acc data copyin(A, B) copyout(C)
-#pragma acc parallel loop tile(64,8) reduction(+:temp)
+  #pragma acc data copyin(A[0:m*p], B[0:p*q]) copyout(C[0:m*q])
+  {
+  #pragma acc parallel loop tile(64,8) reduction(+:temp)
   for (int i = 0; i < m; i++) {
     for (int j = 0; j < q; j++) {
       temp = 0.0;
-#pragma acc loop reduction(+:temp)
+      #pragma acc loop reduction(+:temp)
       for (int k = 0; k < p; k++) {
         temp += A[i*p+k] * B[k*q+j];
       }
       C[i*q+j] = temp;
       }
+  }
   }
 }
