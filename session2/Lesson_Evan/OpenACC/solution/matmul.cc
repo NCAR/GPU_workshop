5312075cfa288fb13a3aa1cfa950990fd1c78ff7
Evan MacBride <ejmacbride@gmail.com>
2021-02-04 15:11:03 -0700

    add gang vector to outer loop
    Add gang vector clauses to outer loop. Also remove reduction from
    inner loop.

--- session2/Lesson_Evan/OpenACC/solution/matmul.cc
+++ session2/Lesson_Evan/OpenACC/solution/matmul.cc
@@ -4,11 +4,11 @@ void gpuMatmul(const float *A, const float *B, float *C, const int m, const int 
   float temp = 0.0;
   #pragma acc data copyin(A[0:m*p], B[0:p*q]) copyout(C[0:m*q])
   {
-  #pragma acc parallel loop tile(64,8) reduction(+:temp)
+  #pragma acc parallel loop gang vector tile(64,8) reduction(+:temp)
   for (int i = 0; i < m; i++) {
     for (int j = 0; j < q; j++) {
       temp = 0.0;
-      #pragma acc loop reduction(+:temp)
+      #pragma acc loop
       for (int k = 0; k < p; k++) {
         temp += A[i*p+k] * B[k*q+j];
       }
