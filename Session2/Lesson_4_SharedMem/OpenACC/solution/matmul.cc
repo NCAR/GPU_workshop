22199f7310c607a0d016ae5083f3ae831b8ffa0e
G. Dylan Dickerson <gdicker@ucar.edu>
2021-04-05 15:17:56 -0600

    Restructing directories for NCAR-Wide tutorial

---
+++ Session2/Lesson_4_SharedMem/OpenACC/solution/matmul.cc
@@ -0,0 +1,19 @@
+#include "pch.h"
+
+void gpuMatmul(const float *restrict A, const float *restrict B, float *C, const int m, const int p, const int q) {
+  float temp = 0.0;
+  #pragma acc data copyin(A[0:m*p], B[0:p*q]) copyout(C[0:m*q])
+  {
+  #pragma acc parallel loop gang vector tile(64,8) reduction(+:temp)
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < q; j++) {
+      temp = 0.0;
+      #pragma acc loop seq
+      for (int k = 0; k < p; k++) {
+        temp += A[i*p+k] * B[k*q+j];
+      }
+      C[i*q+j] = temp;
+    }
+  }
+  }
+}
---
+++ Session2/Lesson_4_SharedMem/pgi_compare/exercise/common.cpp
@@ -0,0 +1,106 @@
+/* common.cpp
+ * To contain functions that are common across all lessons
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define RANGE_MAX 1.0 
+#define RANGE_MIN -1.0
+
+/* Sets all values in array equal to val */
+void InitializeMatrixSame(float *array, const int ny, const int nx, const float val, const char* name){
+	// p serves as another pointer to the start rows within array
+	float *p = array;
+
+	for(int i=0; i<ny; i++){
+		for(int j=0; j<nx; j++){
+			p[j] = val;
+		}
+		// Advance p to the next row
+		p += nx;
+	}
+        printf("Initialized Matrix %s, %d X %d \n",name, ny, nx);
+        
+      
+}
+
+/* Sets all elements of array to a number between [RANGE_MIN,RANGE_MAX] */
+void InitializeMatrixRand(float *array, const int ny, const int nx,const char* name){
+	// p serves as another pointer to the start rows within array
+	float *p = array;
+
+	for (int i = 0; i < ny; i++)
+	{
+		for (int j = 0; j < nx; j++)
+		{
+			p[j] = ((float)rand() / (RAND_MAX)*(RANGE_MAX - RANGE_MIN) + RANGE_MIN);
+		}
+		// Advance p to the next row
+		p += nx;
+	}
+        printf("Initialized Random Matrix %s, %d X %d \n",name, ny, nx);
+}
+
+/* Compares the matrices element-wise and prints an error message if 
+ * the difference between values is above fTolerance
+ */
+void MatrixVerification(float *hostC, float *gpuC, const int ny, const int nx, const float fTolerance){
+	// Pointers for rows in each matrix
+	float *p = hostC;
+	float *q = gpuC;
+        bool PassFlag = 1;
+
+	for (int i = 0; i < ny; i++)
+	{
+		for (int j = 0; j < nx; j++)
+		{
+			if (fabs(p[j] - q[j]) > fTolerance)
+			{
+				printf("error: %f > %f", fabs(p[j]-q[j]),fTolerance);
+				printf("\t host_C[%d][%d]= %f", i,j, p[j]);
+				printf("\t GPU_C[%d][%d]= %f", i,j, q[j]);
+                                PassFlag=0;
+				return;
+			}
+		}
+		p += nx;
+		q += nx;
+	}
+        if(PassFlag)
+                        {
+                                printf("Verification passed\n");
+                        }
+}
+
+void PrintMatrix(float *matrix, int ny, int nx){
+	if (ny <= 6 && nx <= 6)
+	{
+		float *p = matrix;
+
+		for (int i = 0; i < ny; i++)
+		{
+			for (int j = 0; j < nx; j++)
+			{
+				printf("%f\t",p[j]);
+			}
+			printf("\n");
+			fflush(stdout);
+			p += nx;
+		}
+	}
+}
+
+void copyMatrix(float *src, float *dest, const int ny, const int nx){
+	float *p = src;
+	float *q = dest;
+	for(int i=0; i<ny; i++){
+		for(int j=0; j<nx; j++){
+			q[j] = p[j];
+		}
+		p += nx;
+		q += nx;
+	}
+}
