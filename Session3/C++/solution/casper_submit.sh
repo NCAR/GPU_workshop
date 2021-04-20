d8e477705e1f95b21f87a2685c322d6d61877d3a
cenamiller <cena.miller@gmail.com>
2021-04-20 13:44:11 -0600

    modifying submit script in exer/sol

--- Session3/C++/solution/casper_submit.sh
+++ Session3/C++/solution/casper_submit.sh
@@ -6,7 +6,7 @@
 #PBS -l walltime=00:10:00
 #PBS -j oe
 #PBS -A NTDD0002
-#PBS -q casper
+#PBS -q R123790
 
 # Load the necessary modules (software)
 module purge
@@ -28,10 +28,17 @@ export UCX_TLS=rc,sm,cuda_copy,cuda_ipc
 export OMPI_MCA_pml=ucx
 export OMPI_MCA_btl=self,vader,tcp,smcuda
 export UCX_MEMTYPE_CACHE=n
-#export UCX_RNDV_SCHEME=get_zcopy
+export UCX_RNDV_SCHEME=put_zcopy
+export UCX_RNDV_THRESH=2
+
 
 # Move to the correct directory and run the executable
 echo -e "\nBeginning code output:\n-------------\n"
 mpirun -n 4 ./mpi_acc_stencil.exe 256
+
+# nvprof command line profiling
 #mpirun  -n 4 nvprof --devices 0 --print-gpu-trace --profile-api-trace none --kernels "::LaplaceJacobi_MPIACC:2"  --metrics "achieved_occupancy,gld_transactions,gst_transactions,flop_count_sp"  ./mpi_acc_stencil.exe 128
+
+## nsys GUI profiling
+#mpirun -n 4 nsys profile -t cuda,openacc,mpi,nvtx --mpi-impl openmpi -o profileData."%q{OMPI_COMM_WORLD_RANK}" ./mpi_acc_stencil.exe 8
 echo -e "\nEnd of code output:\n-------------\n"
