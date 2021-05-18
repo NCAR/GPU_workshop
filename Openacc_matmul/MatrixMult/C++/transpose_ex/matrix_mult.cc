7535ade9f071728de2c4686466e47573bba99879
cenamiller <cena.miller@gmail.com>
2021-05-18 12:55:03 -0600

    Mat Multiply with transpose

---
+++ Openacc_matmul/MatrixMult/C++/transpose_ex/matrix_mult.cc
@@ -0,0 +1,50 @@
+#include <openacc.h>
+#include "pch.h"
+#include <stdio.h>
+
+
+void openacc_matrix_mult(const float *A,const float *B, float *C, const int rowsA, const int colsB, const int rowsB)
+{
+  
+  float temp = 0.0;
+  for (int i = 0; i < rowsA; i++)
+    for (int j = 0; j < colsB; j++)
+    {
+        temp = 0.0;
+        for (int k = 0; k < rowsB; k++)
+        {
+            temp += A[i*rowsB+k] * B[k*colsB+j];
+        }
+
+        C[i*colsB+j] = temp;
+   }
+}
+
+void openacc_matrix_mult_trans(const float *A,const float *B, float *C, const int rowsA, const int colsB, const int rowsB)
+{
+
+ float temp = 0.0;
+ float *t_B;
+
+ t_B = (float*)malloc(rowsB*colsB*sizeof(float));
+
+ for (int i = 0; i < rowsB; i++)
+      for (int j = 0; j < colsB; j++)
+      {
+         t_B[j*rowsB+i] = B[i*colsB+j];
+
+      }
+  for (int i = 0; i < rowsA; i++)
+      for (int j = 0; j < colsB; j++)
+      {
+           temp = 0.0;
+           for (int k = 0; k < rowsB; k++)
+           {
+               temp += A[i*rowsB+k] * t_B[i*rowsB+k];
+           }
+           C[i*colsB+j] = temp;
+      }
+  }
+   free(t_B);
+}
+
