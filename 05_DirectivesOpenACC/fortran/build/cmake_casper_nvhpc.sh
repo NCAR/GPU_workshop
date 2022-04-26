6d2265cf3f46b81c563fb70a0a3ed672f6c2d842
Daniel Howard <dhoward@ucar.edu>
2022-04-26 08:08:45 -0600

    Update default CMake model configuration
    Remove specifying model config in Casper cmake Fortran script

--- 05_DirectivesOpenACC/fortran/build/cmake_casper_nvhpc.sh
+++ 05_DirectivesOpenACC/fortran/build/cmake_casper_nvhpc.sh
@@ -14,12 +14,12 @@ cmake -DCMAKE_Fortran_COMPILER=mpif90               \
       -DDO_CONCURRENT_FLAGS="-stdpar=gpu -Minfo=stdpar -gpu=cc60,cc70"     \
       -DFFLAGS="-O3 -DNO_INFORM"                                \
       -DLDFLAGS=""                                  \
-      -DNX=200 \
-      -DNZ=100 \
-      -DDATA_SPEC="DATA_SPEC_GRAVITY_WAVES" \
-      -DSIM_TIME=600 \
-      -DOUT_FREQ=100 \
       ..
+#      -DNX=200 \
+#      -DNZ=100 \
+#      -DDATA_SPEC="DATA_SPEC_GRAVITY_WAVES" \
+#      -DSIM_TIME=600 \
+#      -DOUT_FREQ=100 \
 
 # make 
 # Use below command instead to make only desired executables
